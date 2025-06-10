e7e2a67aad463af38679a55d2f3fb8a53937db62
Ethan Carollo <118255407+EthanCarollo@users.noreply.github.com>
2025-06-10 18:28:06 +0200

    Chore : add LeanTween

---
+++ Assets/Script/Scenes/SceneTransitor.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+
+public class SceneTransitor : MonoBehaviour
+{
+    public static SceneTransitor Instance;
+    
+    public GameObject loadingScreen;
+
+    public void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        Instance = this;
+        DontDestroyOnLoad(this);
+    }
+    
+    public void LoadScene(int sceneToLoad){
+        var loadingScreenPrefab = GameObject.Instantiate(loadingScreen);
+        loadingScreenPrefab.GetComponent<LoadingScreenManager>().StartToLoadScene(sceneToLoad);
+    }
+    
+    public void LoadScene(int sceneToLoad, Action onEndCallback){
+        var loadingScreenPrefab = GameObject.Instantiate(loadingScreen);
+        loadingScreenPrefab.GetComponent<LoadingScreenManager>().StartToLoadScene(sceneToLoad, onEndCallback);
+    }
+}
